1676cb02832cbcd92202370c7ffd3b25af661ac7
Pradeep Challa <pradeep.challa@centrica.com>
2021-07-05 19:31:49 +0100

    update filters data

--- src/redux/reducer/SearchReducer.js
+++ src/redux/reducer/SearchReducer.js
@@ -14,7 +14,8 @@ export default function SearchReducer(state = initialState, action) {
       return {
         ...state,
         loading: false,
-        searchResults: [...action.payload.bestMatches]
+        searchResults: action.payload.bestMatches,
+        filtersObj: getFilterData(action.payload.bestMatches)
       };
     case type.SEARCH_COMPANY_FAILURE:
       return {
@@ -24,6 +25,22 @@ export default function SearchReducer(state = initialState, action) {
     default:
       return state;
   }
+}
+
+const getUniqueValues = (obj, val, attr, prop) => {
+  return !obj[attr] ? [val[prop]] : !obj[attr].includes(val[prop]) ? obj[attr].concat(val[prop]) : obj[attr];
+}
+
+const getFilterData = (arr) => {
+  return arr.reduce((obj, val) => {
+    return {
+      type: getUniqueValues(obj, val, 'type', '3. type'),
+      region: getUniqueValues(obj, val, 'region', '4. region'),
+      timeZone: getUniqueValues(obj, val, 'timeZone', '7. timezone'),
+      currency: getUniqueValues(obj, val, 'currency', '8. currency'),
+      matchScore: getUniqueValues(obj, val, 'matchScore', '9. matchScore')
+    };
+  }, {});
 }
 
 export const getSearchData = state => state.SearchReducer;
