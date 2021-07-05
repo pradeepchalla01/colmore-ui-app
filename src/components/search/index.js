c87e9b0b7f56e1a23bb72138af4d454d70784d47
Pradeep Challa <pradeep.challa@centrica.com>
2021-07-05 21:51:04 +0100

    update filters for search results

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -3,6 +3,7 @@ import { Form, Button, Row, Col, Spinner, Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
 import { isApiKeyAvailable } from "../../util";
+import { FiltersObject } from "../../constants";
 import { searchCompany } from '../../redux/actions/SearchAction';
 import { getSearchData } from '../../redux/reducer/SearchReducer';
 
@@ -11,6 +12,8 @@ const Search = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [keyword, setKeyword] = useState('');
+  const [filterObj, setFilterObj] = useState({});
+  const [filteredResults, setFilteredResults] = useState(searchData.searchResults || []);
 
   useEffect(() => {
     isApiKeyAvailable(history);
@@ -22,20 +25,59 @@ const Search = () => {
     }
   }, [dispatch, keyword]);
 
+  useEffect(() => {
+    setFilteredResults(searchData.searchResults)
+  }, [searchData]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setKeyword('');
   }
 
-  console.log(searchData);
   const selectCompany = (selectedCompany) => {
     history.push(`/details/${selectedCompany['1. symbol']}`);
   }
 
+  const toggleChange = (key, value) => {
+    const filter = { ...filterObj };
+    if (!filter[key]) {
+      filter[key] = [value];
+    } else {
+      const index = filter[key]?.indexOf(value);
+      if (index > -1) {
+        filter[key].splice(index, 1);
+        if(!filter[key]?.length) {
+          delete filter[key];
+        }
+      } else {
+        filter[key].push(value);
+      }
+    }
+    setFilterObj(filter);
+  }
+
+  useEffect(() => {
+    const filteredData = searchData.searchResults && [...searchData.searchResults];
+    const updatedData = filteredData?.length && filteredData.filter((obj) => {
+      if (Object.keys(filterObj)?.length) {
+        return Object.keys(filterObj).some((key) => {
+          if (filterObj[key].length) {
+            return filterObj[key].includes(obj[key]);
+          } else {
+            return true;
+          }
+        });
+      } else {
+        return true;
+      }
+    });
+    setFilteredResults(updatedData);
+  }, [filterObj, searchData.searchResults]);
+
   return (
     <>
       <Row className="mb-4">
-        <Col md={9}>
+        <Col md={10}>
           <Form onSubmit={handleSubmit}>
             <h2>Search Page</h2>
             <Form.Control type="text" placeholder="Search company" required value={keyword} onInput={e => setKeyword(e.target.value)} />
@@ -45,19 +87,37 @@ const Search = () => {
       </Row>
       {searchData.loading &&
         <Row>
-          <Col md={9}>
+          <Col md={10}>
             <Spinner animation="border" role="status">
               <span className="sr-only">Loading...</span>
             </Spinner>
           </Col>
         </Row>
       }
-      {searchData.searchResults &&
+      {filteredResults &&
         <Row>
           <Col md={3}>
-
+            <h3 className="text-left mb-4">Filters</h3>
+            {searchData.filtersObj &&
+              Object.keys(searchData.filtersObj).map((key, index) => (
+                searchData.filtersObj[key]?.length &&
+                (<Col md={12} className="text-left mb-4" key={index}>
+                  <h4>{FiltersObject[key].val}</h4>
+                  <Col md={12} style={{ display: 'grid', gridAutoColumns: 'max-content' }}>
+                    {
+                      searchData.filtersObj[key].map((value, i) => (
+                        <div key={i}>
+                          <input type="checkbox" style={{ marginRight: '5px' }} onChange={() => toggleChange(FiltersObject[key].prop, value)} />
+                          {value}
+                        </div>
+                      ))
+                    }
+                  </Col>
+                </Col>)
+              ))
+            }
           </Col>
-          <Col md={6}>
+          <Col md={7}>
             <Table>
               <thead>
                 <tr>
@@ -68,7 +128,7 @@ const Search = () => {
                 </tr>
               </thead>
               <tbody>
-                {searchData.searchResults.map((item, index) => (
+                {filteredResults.map((item, index) => (
                   <tr key={index} style={{ cursor: "pointer" }} onClick={() => selectCompany(item)}>
                     <td>{index + 1}</td>
                     <td>{item['2. name']}</td>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,14 @@
 export const getApiKey = () => {
-    return window.sessionStorage.getItem('api_key');
+  return window.sessionStorage.getItem('api_key');
 };
 export const setApiKey = (key) => {
-    window.sessionStorage.setItem('api_key', key);
+  window.sessionStorage.setItem('api_key', key);
 };
 
+export const FiltersObject = {
+  type: { prop: '3. type', val: 'Type' },
+  region: { prop: '4. region', val: 'Region' },
+  timeZone: { prop: '7. timezone', val: 'TimeZone' },
+  currency: { prop: '8. currency', val: 'Currency' },
+  matchScore: { prop: '9. matchScore', val: 'Match Score' }
+}
